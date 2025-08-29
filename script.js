47597e356252ddf08095cac02031021236a9f82a
iya183 <iyappanfintech@gmail.com>
2025-08-29 20:09:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+  
+ 
+  document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+  });
+  
